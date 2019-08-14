96920dd80a3cf1f45754d5a8e7f4bd7cba460eae
janardhansivakula <sivakulajanardhan259@gmail.com>
2019-08-14 12:56:42 +0530

    taking files

---
+++ true1.java
@@ -0,0 +1,16 @@
+class true1 
+{
+	int a,b,c;
+	true1(int a,int b)
+	{
+		System.out.println("values of a="+a");
+		System.out.println("values of b="+b);
+	//	System.out.println("vlaue of c="+c);
+
+	public static void main(String[] args) 
+	{
+		
+		True1 t1=new True1();
+		//System.out.println("Hello World!");
+	}
+}
